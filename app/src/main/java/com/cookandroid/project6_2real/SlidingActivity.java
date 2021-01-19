7b217bd0d36622181d366c13f274e86a0db8909a
desirel <two_somang@icloud.com>
2021-01-20 01:04:40 +0900

    Initial commit

---
+++ app/src/main/java/com/cookandroid/project6_2real/SlidingActivity.java
@@ -0,0 +1,14 @@
+package com.cookandroid.project6_2real;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SlidingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding);
+    }
+}
